1877ee94981a6c9fc91ecbf2a2583e524fccca64
Hammy Havoc <hammy@previousmagazine.com>
2017-06-21 02:06:41 +0100

    Improved config.

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ define( function ( require ) {
 		app_slug : 'collective-misfits-android',
 		wp_ws_url : 'http://www.com-ent.com/wp-appkit-api/collective-misfits-android',
 		wp_url : 'http://www.com-ent.com',
-		theme : 'q-android-master',
+		theme : 'q-android',
 		version : '1.0',
 		app_title : 'Collective of Misfits',
 		app_platform : 'android',
